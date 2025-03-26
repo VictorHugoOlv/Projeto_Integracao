a4bb5d956317c65ef7e1a30b18a79d4d4eaa3166
Victor Hugo <victor.hugo.olv@gmail.com>
2025-03-26 14:59:58 -0300

    CategoryModels modificado para ProductCategory

--- src/main/java/com/example/projetointegracao/ProductCategory.java
+++ src/main/java/com/example/projetointegracao/ProductCategory.java
@@ -1,11 +1,11 @@
 package com.example.projetointegracao;
 
 // Classe auxiliar para armazenar Categoria e Modelo
-public class CategoryModel {
+public class ProductCategory {
     private final String category;
     private final String model;
 
-    public CategoryModel(String category, String model) {
+    public ProductCategory(String category, String model) {
         this.category = category;
         this.model = model;
     }
