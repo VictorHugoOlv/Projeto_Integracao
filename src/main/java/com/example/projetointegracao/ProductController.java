721c5f13e06429eba55afdce920a2672a170f255
Victor Hugo <victor.hugo.olv@gmail.com>
2025-03-26 17:49:17 -0300

    Alteração classes e metódos: ProductLinesController, ProductLineModels, productLinesList e loadProductLines

--- src/main/java/com/example/projetointegracao/ProductController.java
+++ src/main/java/com/example/projetointegracao/ProductController.java
@@ -6,9 +6,9 @@ import javafx.scene.control.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class ProductLinesController {
+public class ProductController {
 
-    private List<ProductLinesModel> productLinesList = new ArrayList<>();
+    private List<Product> productList = new ArrayList<>();
 
     @FXML
     private TreeView<String> modelsTreeView;
@@ -23,7 +23,7 @@ public class ProductLinesController {
     @FXML
     public void initialize(){
 
-        loadProductLines();
+        loadProductList();
         selectionComboBox.getItems().addAll(getDistinctLinesSorted());
 
         selectionComboBox.setOnAction(event -> {
@@ -38,35 +38,35 @@ public class ProductLinesController {
     }
 
 
-    public List<ProductLinesModel> loadProductLines() {
-        productLinesList.add(new ProductLinesModel("Cronos", "CronosOld", "Cronos 6001‑A"));
-        productLinesList.add(new ProductLinesModel("Cronos", "CronosOld", "Cronos 6003"));
-        productLinesList.add(new ProductLinesModel("Cronos", "CronosOld", "Cronos 7023"));
+    public List<Product> loadProductList() {
+        productList.add(new Product("Cronos", "CronosOld", "Cronos 6001‑A"));
+        productList.add(new Product("Cronos", "CronosOld", "Cronos 6003"));
+        productList.add(new Product("Cronos", "CronosOld", "Cronos 7023"));
 
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos L", "Cronos 6021L"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos L", "Cronos 7023L"));
+        productList.add(new Product("Cronos", "Cronos L", "Cronos 6021L"));
+        productList.add(new Product("Cronos", "Cronos L", "Cronos 7023L"));
 
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 6001‑NG"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 6003‑NG"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 6021‑NG"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 6031‑NG"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 7021‑NG"));
-        productLinesList.add(new ProductLinesModel("Cronos", "Cronos‑NG", "Cronos 7023‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 6001‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 6003‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 6021‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 6031‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 7021‑NG"));
+        productList.add(new Product("Cronos", "Cronos‑NG", "Cronos 7023‑NG"));
 
-        productLinesList.add(new ProductLinesModel("Ares", "Ares TB", "ARES 7021"));
-        productLinesList.add(new ProductLinesModel("Ares", "Ares TB", "ARES 7031"));
-        productLinesList.add(new ProductLinesModel("Ares", "Ares TB", "ARES 7023"));
+        productList.add(new Product("Ares", "Ares TB", "ARES 7021"));
+        productList.add(new Product("Ares", "Ares TB", "ARES 7031"));
+        productList.add(new Product("Ares", "Ares TB", "ARES 7023"));
 
-        productLinesList.add(new ProductLinesModel("Ares", " Ares THS", "ARES 8023 15"));
-        productLinesList.add(new ProductLinesModel("Ares", " Ares THS", "ARES 8023 200"));
-        productLinesList.add(new ProductLinesModel("Ares", " Ares THS", "ARES 8023 2,5"));
+        productList.add(new Product("Ares", " Ares THS", "ARES 8023 15"));
+        productList.add(new Product("Ares", " Ares THS", "ARES 8023 200"));
+        productList.add(new Product("Ares", " Ares THS", "ARES 8023 2,5"));
 
-        return productLinesList;
+        return productList;
     }
 
     public Set<String> getDistinctLinesSorted() {
-        return productLinesList.stream()
-                .map(ProductLinesModel::getLine)
+        return productList.stream()
+                .map(Product::getLine)
                 .filter(line -> !line.isEmpty())
                 .distinct()
                 .sorted()
@@ -79,7 +79,7 @@ public class ProductLinesController {
         Set<ProductCategory> productCategories = new HashSet<>();
 
         // Filtra as categorias e modelos para a linha selecionada
-        productLinesList.stream()
+        productList.stream()
                 .filter(p -> p.getLine().equals(selectedLine))  // Filtra pela linha selecionada
                 .forEach(productLine -> {
                     if (!productLine.getCategory().isEmpty() && !productLine.getModel().isEmpty()) {
