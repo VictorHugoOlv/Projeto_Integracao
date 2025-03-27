9f918549dc4d08275842471868db713fa52ee465
Victor Hugo <victor.hugo.olv@gmail.com>
2025-03-27 09:49:02 -0300

    Modificação: hello-view.fxml para product-page.fxml

--- src/main/java/com/example/projetointegracao/MainApplication.java
+++ src/main/java/com/example/projetointegracao/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/product-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 820, 440);
         stage.setTitle("Projeto de Integração");
         stage.setScene(scene);
